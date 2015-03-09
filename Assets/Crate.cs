153c83cfcee4e29c88ed89849937b34321a61eb6
BATzerk <bretttaylor@Bretts-MacBook-Air.local>
2015-03-09 15:19:04 -0400

    Added more physics
    Added a crate
    Added PhysicsMaterial2Ds for each material
    Added player faces a direction

---
+++ Assets/Crate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crate : MonoBehaviour {
+	// Properties
+	[SerializeField]
+	float mass = 4;
+
+	void Start () {
+		// Set mass!
+		Rigidbody2D rigidbody = GetComponent<Rigidbody2D> ();
+		rigidbody.mass = mass;
+	}
+}
--- Assets/Scripts/GameObjects/Player.cs
+++ Assets/Scripts/GameObjects/Player.cs
@@ -10,13 +10,21 @@ public class Player : MonoBehaviour {
 	float JUMP_FORCE = -600;
 	float GRAVITY_FORCE = -26;
 	// References
-	Rigidbody2D rigidbody;
+	Rigidbody2D rigidbody; // My physics body
+	GameObject bodySprites; // All images that'll be scaled by the direction I'm facing are in here.
 	// Properties
 	bool isGrounded = true;
+	int directionFacing = 1; // Where I'm facing. -1 is left and 1 is right. It determines my X scale. No other values should be used.
 
 	void Start () {
 		// Identify components!
 		rigidbody = (GetComponent<Rigidbody2D> ());
+		foreach (Transform t in transform) {
+			//			if (t.name == "BodySprites") bodySprites = t.GetComponent<GameObject>();
+			if (t.name == "BodySprites") bodySprites = t.gameObject;
+		}
+
+		rigidbody.mass = 0.2f;
 	}
 
 	void Update () {
@@ -35,15 +43,24 @@ public class Player : MonoBehaviour {
 		rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y+GRAVITY_FORCE);
 	}
 	void InputLogic() {
-		// Accept player input to influence my velocity.
-		// Horizontal
 		float inputX = Input.GetAxis ("Horizontal");
+
+		// VELOCITY
+		//	Horizontal
 		Vector3 newVelocity = new Vector2(rigidbody.velocity.x+inputX*movementSpeed, rigidbody.velocity.y);
 		rigidbody.velocity = newVelocity;
-		// Jump
+		//	Jump
 		if (Input.GetButtonDown ("Jump")) {
 			Jump();
 		}
+
+		// DIRECTION
+		//	Update directionFacing if the horizontal input is significant enough!
+		if (Mathf.Abs (inputX) > 0.05f) {
+			directionFacing = Sign(inputX);
+		}
+		//	Apply scale!
+		bodySprites.transform.localScale = new Vector2(directionFacing, bodySprites.transform.localScale.y);
 	}
 	void ApplyFriction() {
 		// If I'm on the ground, apply basic ground friction!
@@ -61,6 +78,15 @@ public class Player : MonoBehaviour {
 
 	void Jump() {
 		rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y-JUMP_FORCE);
+	}
+
+
+
+
+	int Sign(float value) {
+		if (value < 0) return -1;
+		if (value > 0) return 1;
+		return 0;
 	}
 }
 

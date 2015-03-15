248bd8e421f42a61c1874123880be0025bc3cdec
BATzerk <bretttaylor@Bretts-MacBook-Air.local>
2015-03-15 10:26:39 -0400

    Added HOLDING/MOVING crate!
    + Player can basically hold onto the crate and move it around!
    * Renamed “grabbing” to “holding”

--- Assets/Scripts/GameObjects/Crate.cs
+++ Assets/Scripts/GameObjects/Crate.cs
@@ -2,31 +2,41 @@
 using System.Collections;
 
 public class Crate : MonoBehaviour {
-	// Properties
-//	[SerializeField]
-//	float mass = 4;
+	// Constants
+	private const float GAP_TO_PLAYER = 0f;
 	// References
 	private SpriteRenderer spriteRenderer;
+	private Rigidbody2D rigidbody;
+	private Player myPlayerRef; // which player is grabbing me. Passed in on onGrabbing.
 	// Properties
-	private bool isGrabbing; // actually being grabbed right now
 	private bool isGrabbable; // theoretically grabbable if the player hits SHIFT key
+	private float bodyWidth; // how wide this crate is! Currently based on my sprite's size :P
+	private float holdingOffsetX; // the distance between me and myPlayerRef when I'm being held.
+
+	// Getters
+	private bool IsBeingHeld { get { return myPlayerRef != null; } } // actually being held/grabbed/dragged/sensually caressed right now
+	public Rigidbody2D MyRigidbody { get { return rigidbody; } }
+
 
 	void Start () {
 		// Set mass!
-		Rigidbody2D rigidbody = GetComponent<Rigidbody2D> ();
+		rigidbody = GetComponent<Rigidbody2D> ();
 //		rigidbody.mass = mass;
 
 		spriteRenderer = GetComponentInChildren<SpriteRenderer> ();
 
+		// DEBUG/TEMPORARY stuff
+		bodyWidth = spriteRenderer.bounds.size.x;
+
 		// Set initial values
-		isGrabbing = false;
 		isGrabbable = false;
+		myPlayerRef = null;
 
 		SetColorBasedOnGrabVariables ();
 	}
 
 	private void SetColorBasedOnGrabVariables() {
-		if (isGrabbing) spriteRenderer.renderer.material.color = Color.yellow;
+		if (IsBeingHeld) spriteRenderer.renderer.material.color = Color.yellow;
 		else if (isGrabbable) spriteRenderer.renderer.material.color = Color.cyan;
 		else spriteRenderer.renderer.material.color = Color.blue;
 	}
@@ -40,13 +50,35 @@ public class Crate : MonoBehaviour {
 		SetColorBasedOnGrabVariables ();
 	}
 	
-	public void OnGrabbing() {
-		isGrabbing = true;
+	public void OnGrabbed(Player thisPlayer) {
+		myPlayerRef = thisPlayer;
+		SetColorBasedOnGrabVariables ();
+		// Determine holdingOffsetX!
+		float targetPosX = myPlayerRef.MyRigidbody.position.x;
+		if (rigidbody.position.x < myPlayerRef.MyRigidbody.position.x) {
+			holdingOffsetX = -(myPlayerRef.BodyWidth + this.bodyWidth + GAP_TO_PLAYER) * 0.5f;
+		}
+		else {
+			holdingOffsetX = (myPlayerRef.BodyWidth + this.bodyWidth + GAP_TO_PLAYER) * 0.5f;
+		}
+	}
+	public void OnUngrabbed() {
+		myPlayerRef = null;
 		SetColorBasedOnGrabVariables ();
 	}
-	public void OnUngrabbing() {
-		isGrabbing = false;
-		SetColorBasedOnGrabVariables ();
+
+
+
+	void FixedUpdate() {
+		CrateHoldingMath ();
+	}
+	
+	void CrateHoldingMath() {
+		if (myPlayerRef == null) { return; }
+		// Set my position!
+		// HACKY/TEMPORARY: this constant on player's velocity was totally eyeballed. Might be COMPLETELY off.
+		float targetPosX = (myPlayerRef.MyRigidbody.position.x+holdingOffsetX) + myPlayerRef.MyRigidbody.velocity.x/60;
+		rigidbody.position = new Vector2(targetPosX, rigidbody.position.y);
 	}
 }
 
--- Assets/Scripts/GameObjects/Player.cs
+++ Assets/Scripts/GameObjects/Player.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	// ================================
+	//	Properties
+	// ================================
 	// Settables
 	float movementSpeed = 200f;
 	float maxVelX = 300;
@@ -16,10 +19,20 @@ public class Player : MonoBehaviour {
 	PlayerFeetSensor feetSensor; // The sensor at my "feet" that determines if I'm on ground.
 	PlayerHandSensor handSensor; // The sensor at my "hands" that determines if I can grab something in front of me.
 	GameObject DEBUG_bodySprite; // JUST the body
-	Crate crateGrabbing;
+	Crate crateHolding;
 	// Properties
+	float bodyWidth; // it's exactly how wide the sprite is! Currently used for offseting crates' positions.
 	int directionFacing = 1; // Where I'm facing. -1 is left and 1 is right. It determines my X scale. No other values should be used.
 
+	// Getters
+	private bool IsHoldingCrate { get { return crateHolding != null; } }
+	public float BodyWidth { get { return bodyWidth; } }
+	public Rigidbody2D MyRigidbody { get { return rigidbody; } }
+	
+	
+	// ================================
+	//	Start
+	// ================================
 	void Start () {
 		// Identify components!
 		rigidbody = (GetComponent<Rigidbody2D> ());
@@ -28,32 +41,27 @@ public class Player : MonoBehaviour {
 			else if (t.name == "HandSensor") handSensor = t.GetComponent<PlayerHandSensor>();
 		}
 
+		// DEBUG testing stuff. NOTE THAT A LOTTA THIS can't just be removed but will need to be replaced with "proper" implementations
 		DEBUG_bodySprite = GameObject.Find ("Body");
-
+		bodyWidth = DEBUG_bodySprite.renderer.bounds.size.x;
 		rigidbody.mass = 0.2f;
 
 		// Set initial values
-		SetCrateGrabbing (null);
+		SetCrateHolding (null);
 	}
-
-	void FixedUpdate () {
-		ApplyGravity ();
+	
+	
+	// ================================
+	//	Update
+	// ================================
+	void Update() {
 		InputLogicMovement ();
 		InputLogicGrabbingCrates ();
-		ApplyFriction ();
-		TerminalVelocity ();
-
-		if (crateGrabbing != null) DEBUG_bodySprite.renderer.material.color = Color.blue;//feetSensor.IsGrounded
-		else DEBUG_bodySprite.renderer.material.color = Color.cyan;
 	}
-
-
-
-	void ApplyGravity() {
-		//if (!isGrounded) {
-			rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y+GRAVITY_FORCE);
-		//}
-	}
+	
+	// ================================
+	//	Input Functions
+	// ================================
 	void InputLogicMovement() {
 		float inputX = Input.GetAxis ("Horizontal");
 		
@@ -67,28 +75,69 @@ public class Player : MonoBehaviour {
 		}
 		
 		// DIRECTION
-		//	Update directionFacing if the horizontal input is significant enough!
-		if (Mathf.Abs (inputX) > 0.05f) {
-			directionFacing = Sign(inputX);
+		//	Holding crate?!
+		if (IsHoldingCrate) {
+			// directionFacing will be respective to the crate being left/right of me.
+			directionFacing = Sign (crateHolding.MyRigidbody.position.x - rigidbody.position.x);
 		}
-		//	Apply scale!
-		this.transform.localScale = new Vector2(directionFacing, this.transform.localScale.y);
-	}
-	void InputLogicGrabbingCrates() {
-		// NOT grabbing a crate...
-		if (crateGrabbing == null) {
-			// NEXT to a crate and just hit GRAB key?!
-			if (handSensor.CrateTouching!=null && Input.GetKeyDown(KEYCODE_CRATE_GRAB)) {
-				SetCrateGrabbing(handSensor.CrateTouching);
+		//	NOT holding crate...
+		else {
+			//	Update directionFacing if the horizontal input is significant enough!
+			if (Mathf.Abs (inputX) > 0.05f) {
+				directionFacing = Sign(inputX);
 			}
 		}
-		// YES grabbing a crate!
+		//	Apply scale for direction!
+		this.transform.localScale = new Vector2(directionFacing, this.transform.localScale.y);
+	}
+
+	void Jump() {
+		// If I'm on the ground, AND I'm not holding a crate...
+		if (feetSensor.IsGrounded && crateHolding==null) {
+			// JUMP!
+			rigidbody.velocity = new Vector2 (rigidbody.velocity.x, rigidbody.velocity.y - JUMP_FORCE);
+		}
+	}
+
+	void InputLogicGrabbingCrates() {
+		// NOT holding a crate...
+		if (crateHolding == null) {
+			// NEXT to a crate and just hit GRAB key?!
+			if (handSensor.CrateTouching!=null && Input.GetKeyDown(KEYCODE_CRATE_GRAB)) {
+				SetCrateHolding(handSensor.CrateTouching);
+			}
+		}
+		// YES holding a crate!
 		else {
 			// Just RELEASED GRAB key?!
 			if (Input.GetKeyUp(KEYCODE_CRATE_GRAB)) {
-				SetCrateGrabbing(null);
+				SetCrateHolding(null);
 			}
 		}
+	}
+
+
+	
+	// ================================
+	//	Fixed Update
+	// ================================
+	void FixedUpdate () {
+		ApplyGravity ();
+		ApplyFriction ();
+		TerminalVelocity ();
+
+		if (crateHolding != null) DEBUG_bodySprite.renderer.material.color = Color.blue;//feetSensor.IsGrounded
+		else DEBUG_bodySprite.renderer.material.color = Color.cyan;
+	}
+
+	
+	// ================================
+	//	Physics-themed Methods
+	// ================================
+	void ApplyGravity() {
+		//if (!isGrounded) {
+			rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y+GRAVITY_FORCE);
+		//}
 	}
 	void ApplyFriction() {
 		// If I'm on the ground, apply basic ground friction!
@@ -104,29 +153,28 @@ public class Player : MonoBehaviour {
 		);
 	}
 
-	void Jump() {
-		// If I'm on the ground, then jump!
-		if (feetSensor.IsGrounded) {
-			rigidbody.velocity = new Vector2 (rigidbody.velocity.x, rigidbody.velocity.y - JUMP_FORCE);
+
+	// ================================
+	//	Crates
+	// ================================
+	void SetCrateHolding(Crate tempCrate) {
+		// FIRST, if I'm already HOLDING a crate, tell it it's ungrabbed!
+		if (crateHolding != null) {
+			crateHolding.OnUngrabbed ();
+		}
+		// Set new crateHolding!
+		crateHolding = tempCrate;
+		// Notify crateHolding that it's been grabbed!
+		if (crateHolding != null) {
+			crateHolding.OnGrabbed (this);
 		}
 	}
 
-
-	void SetCrateGrabbing(Crate tempCrate) {
-		// FIRST, if I'm already GRABBING a crate, tell it it's ungrabbed!
-		if (crateGrabbing != null) {
-			crateGrabbing.OnUngrabbing ();
-		}
-		// Set new crateGrabbing!
-		crateGrabbing = tempCrate;
-		// Notify crateGrabbing that it's being grabbed!
-		if (crateGrabbing != null) {
-			crateGrabbing.OnGrabbing ();
-		}
-	}
-
-
-
+	
+	
+	// ================================
+	//	Utility Functions
+	// ================================
 	int Sign(float value) {
 		if (value < 0) return -1;
 		if (value > 0) return 1;
